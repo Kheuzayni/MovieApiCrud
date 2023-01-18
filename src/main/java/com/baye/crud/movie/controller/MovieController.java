00b517f96f81143dca528b9312a70fa194d89f5e
unknown <bayecheikhfall249@gmail.com>
2023-01-18 01:12:39 +0000

    controller

--- src/main/java/com/baye/crud/movie/controller/MovieController.java
+++ src/main/java/com/baye/crud/movie/controller/MovieController.java
@@ -1,4 +1,36 @@
 package com.baye.crud.movie.controller;
 
+import com.baye.crud.movie.modele.Movie;
+import com.baye.crud.movie.service.MovieService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+@AllArgsConstructor
 public class MovieController {
+
+    private final MovieService movieService;
+
+    @PostMapping("/create")
+    public Movie create(@RequestBody Movie movie){
+        return movieService.creer(movie);
+    }
+
+    @GetMapping("/read")
+    public List<Movie> read(){
+        return movieService.lire();
+    }
+
+    @PutMapping("/update/{id}")
+    public Movie update(@PathVariable Long id,@RequestBody Movie movie){
+        return movieService.modifier(id, movie);
+    }
+
+    @DeleteMapping("/delete")
+    public String delete(@PathVariable Long id){
+        return movieService.supprimer(id);
+    }
 }
